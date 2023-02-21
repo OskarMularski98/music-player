9fe396f1c8cdd28a6e0d6bc8107da1da986846fc
Oskar Mularski <oskarmularski98@gmail.com>
2023-02-21 11:58:31 +0100

    setup project

---
+++ src/util.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { v4 as uuid4 } from "uuid";
+
+const chillHop = () => {
+  return [
+    {
+      name: "Dreaming",
+      artist: "Misha, Jussi Halme, El Train, goodie bags",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49425",
+    },
+    {
+      name: "Kaleidoscope",
+      artist: "El Train",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49426",
+    },
+    {
+      name: "Lavender",
+      artist: "El Train",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49428",
+    },
+    {
+      name: "Snap Back",
+      artist: "El Train, Marc Mathias",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49429",
+    },
+    {
+      name: "Sippin'",
+      artist: "El Train, Paal Singh",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49436",
+    },
+    {
+      name: "Slow Burn",
+      artist: "El Train",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49437",
+    },
+    {
+      name: "Cosmos",
+      artist: "El Train, Producerboibrad.",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49438",
+    },
+    {
+      name: "Spring",
+      artist: "El Train, goodie bags",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49439",
+    },
+    {
+      name: "Up Early",
+      artist: "El Train, C Tivey",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49440",
+    },
+    {
+      name: "Crush",
+      artist: "El Train",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      audio: "",
+      color: ["#3C4541", "#EDD6A1"],
+      id: uuid4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49441",
+    },
+  ];
+};
+
+export default chillHop;
